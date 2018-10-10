f9a659f12b7a5828952893ae99764cc8d9ddcb91
njalsson <njalsson1337@gmail.com>
2018-10-10 21:22:00 +0000

    fix

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+function setup(){
+	createCanvas(500,500);
+	translate(250,250);
+	rotate(PI);
+}
+let x;
+let y;
+let equation;
+let newequation;
 
-
-// check if button was pressed, to the event thing, shiiiet
-ctx.beginPath();
-ctx.moveTo(0,250);
-ctx.lineTo(500,250);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(250, 0);
-ctx.lineTo(250, 500);
-ctx.stroke();
-let maxnum;
-let minnum;
-ctx.translate(250,250);
-
-$('#button').click(function() { 
+$('#button').click(function() { 101
 
 
 
@@ -24,52 +16,27 @@ $('#button').click(function() {
 	console.log(equation);
 
 
-	let x;
-	let newx;
-	let y;
-	let newy;
-	let newequation = equation;
-	newequation = newequation.replace("^","**");
-	for(let i = -250; i < 250; i += 0.1){
-		x = i;
-		newequation = equation.replace("x", x);
-		newequation = equation.replace("^", "**");
+	smooth();
+	noFill();
+	beginShape();
+	for(x = -250; x < 250; x+=1){
+		newequation = equation;
+		newequation = newequation.replace("-x", "\(x\)");
+		newequation = newequation.replace("x",x);
+		newequation = newequation.replace("^", "**");
+		console.log(newequation);
 		console.log(x, eval(newequation));
-			y = eval(newequation);
 			// draw, gotta have old x, y, to new x, y
-			newx = x+1;
-			newequation = equation.replace("x", newx);
-			newequation = equation.replace("^", "**");
-			newy = eval(newequation);
-			ctx.beginPath();
-
-			ctx.moveTo(x, y);
-			ctx.lineTo(newx, newy);
-			ctx.stroke();		
-		if (i == -249) { minnum = x }
-		if (i ==  249) { maxnum = x }
-					
+		y = eval(newequation);		
+		curveVertex(x,y);			
 	}
-	let max = document.getElementById("max");
-	let min = document.getElementById("min");
+	endShape();
 	
 });
 
 
 $('#clearbutton').click(function(){
 let canvas = document.getElementById("myCanvas");
-	ctx.translate(-250,-250);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
-ctx.moveTo(0,250);
-ctx.lineTo(500,250);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(250, 0);
-ctx.lineTo(250, 500);
-ctx.stroke();
-	ctx.translate(250,250);
-
+	clear();
 
 });
